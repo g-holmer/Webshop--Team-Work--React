6427cffbc3b110ffdedeb66621b25110a8190054
LinaLin123 <jlina.lin@gmail.com>
2020-09-01 10:32:52 +0200

    jing was here

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          Jing Jing was here
         </p>
         <a
           className="App-link"
