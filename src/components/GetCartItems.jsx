a8f7df3109e75dcd2095d9b2129d80a3cd4864bd
anra1901 <am.ram06@hotmail.com>
2020-09-03 11:59:05 +0200

    Done rendering products to cart

--- src/components/GetCartItems.jsx
+++ src/components/GetCartItems.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default function GetCartItems() {
+export default function GetCartItems({}) {
+  let [cartItems, setCartItems] = useState([]);
 
-    
+  const getItemFromLocalStorage = () => {
+    const allProducts = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const productFromLS = JSON.parse(
+        localStorage.getItem(localStorage.key(i))
+      );
+      allProducts.push(productFromLS);
+    }
 
-    return (
+    setCartItems(allProducts);
+  };
+
+  const productsToRender =
+    cartItems &&
+    cartItems.map((item) => {
+      console.log(cartItems);
+      const itemName = item.name;
+      const itemPrice = item.price;
+      const itemImage = item.images[0].src.small;
+
+      return (
         <div>
-            
+          <ul>
+            <img src={itemImage} alt="" />
+            <li>{itemName}</li>
+            <li>Price: {itemPrice} $</li>
+          </ul>
         </div>
-    )
+      );
+    });
+
+  useEffect(() => {
+    getItemFromLocalStorage();
+  }, []);
+
+  return (
+    <div>
+      <div>{productsToRender}</div>
+    </div>
+  );
 }
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,43 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import GetCartItems from "../components/GetCartItems";
 
 export default function CartPage({}) {
-
-  let [cartItems, setCartItems] = useState([]);
-
-  const getItemFromLocalStorage = () => {
-    const allProducts = [];
-    for (let i = 0; i < localStorage.length; i++) {
-      const productFromLS = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    allProducts.push(productFromLS);
-    }
-
-    setCartItems(allProducts);
-  };
-
-  const productsToRender = cartItems && cartItems.map((item) => {
-    console.log(cartItems);
-    const itemName = item.name;
-    const itemPrice = item.price;
-    const itemImage = item.images[0] && item.images[0].src.small;
-
-    return (
-      <div>
-      <ul>
-        <li>{itemName}</li>
-      </ul>
-      <div>{itemImage}</div>
-      </div>
-  )})
-
-
-useEffect(() => {
-  getItemFromLocalStorage()
-}, [])
-
   return (
     <div>
       <h1>Your cart!</h1>
-      <div>{productsToRender}</div>
+      <GetCartItems />
     </div>
   );
 }
